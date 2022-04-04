a601b800cb8f0be5be33d48f7c154660b6d084ad
Florent Vain <florent.vain@gmail.com>
2022-04-04 05:04:01 +0200

    FIx tmp

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceNL Bot Fork for France
-// @namespace    https://github.com/Skeeww/Bot
-// @version      30
+// @namespace    https://github.com/Makorj/Bot
+// @version      31
 // @description  FRANCE
 // @author       NoahvdAa (fork by Skew)
 // @match        https://www.reddit.com/r/place/*
@@ -9,10 +9,11 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://raw.githubusercontent.com/Skeeww/Bot/master/placenlbot.user.js
-// @downloadURL  https://raw.githubusercontent.com/Skeeww/Bot/master/placenlbot.user.js
+// @updateURL    https://raw.githubusercontent.com/Makorj/Bot/master/placenlbot.user.js
+// @downloadURL  https://raw.githubusercontent.com/Makorj/Bot/master/placenlbot.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
+// @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 var socket;
@@ -354,8 +355,14 @@ async function getCurrentImageUrl(id = '0') {
 function getCanvasFromUrl(url, canvas, x = 0, y = 0, clearCanvas = false) {
     return new Promise((resolve, reject) => {
         let loadImage = ctx => {
+            GM.xmlHttpRequest({
+            method: "GET",
+            url: url,
+            responseType: 'blob',
+            onload: function(response) {
+            var urlCreator = window.URL || window.webkitURL;
+            var imageUrl = urlCreator.createObjectURL(this.response);
             var img = new Image();
-            img.crossOrigin = 'anonymous';
             img.onload = () => {
                 if (clearCanvas) {
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -370,7 +377,7 @@ function getCanvasFromUrl(url, canvas, x = 0, y = 0, clearCanvas = false) {
                 }).showToast();
                 setTimeout(() => loadImage(ctx), 3000);
             };
-            img.src = url;
+            img.src = imageUrl;
         };
         loadImage(canvas.getContext('2d'));
     });
